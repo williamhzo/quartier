705efeb260530f95ddff7bafccc0525f36ee805a
William H <william.hermozo@protonmail.com>
2026-02-28 01:34:25 +0100

    replace 100vh with 100dvh for mobile viewport

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function MapLoading() {
   return (
-    <div className="relative h-[calc(100vh-3.5rem)]">
+    <div className="relative h-[calc(100dvh-3.5rem)]">
       <div className="absolute top-3 left-3 z-10 flex gap-2">
         <Skeleton className="h-9 w-40 rounded-md" />
         <Skeleton className="h-9 w-40 rounded-md" />
--- components/map/paris-map.tsx
+++ components/map/paris-map.tsx
@@ -260,7 +260,7 @@ export function ParisMap({ arrondissements, boundaries }: Props) {
   );
 
   return (
-    <div className="relative h-[calc(100vh-3.5rem)]">
+    <div className="relative h-[calc(100dvh-3.5rem)]">
       <div className="absolute top-3 left-3 z-10 flex gap-2">
         <div className="w-40">
           <DimensionSelect value={dimension} onChange={setDimension} />
