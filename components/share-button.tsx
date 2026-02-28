7365cf0e221ba8e3322f7d55a782ee4984ffe0a9
William H <william.hermozo@protonmail.com>
2026-02-28 01:09:24 +0100

    share

--- components/share-button.tsx
+++ components/share-button.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Check, Link as LinkIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useTranslations } from "next-intl";
+import posthog from "posthog-js";
 
 export function ShareButton({
   number,
@@ -18,6 +19,9 @@ export function ShareButton({
   function handleCopy() {
     const url = `${window.location.origin}/paris/${number}`;
     navigator.clipboard.writeText(url).then(() => {
+      posthog.capture("share_link_copied", {
+        arrondissement_number: number,
+      });
       setCopied(true);
       setTimeout(() => setCopied(false), 2000);
     });
@@ -30,11 +34,7 @@ export function ShareButton({
       onClick={handleCopy}
       title={copied ? t("copied") : t("share")}
     >
-      {copied ? (
-        <Check className="size-4" />
-      ) : (
-        <LinkIcon className="size-4" />
-      )}
+      {copied ? <Check className="size-4" /> : <LinkIcon className="size-4" />}
     </Button>
   );
 }
